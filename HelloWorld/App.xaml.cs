135360de6bc9ad313e307eca3afce4a507f84f9c
khoavmed <khoav@medadvisor.com.au>
2020-10-24 21:15:37 +1100

    add app center SDK.

--- HelloWorld/App.xaml.cs
+++ HelloWorld/App.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace HelloWorld
 {
@@ -15,6 +18,9 @@ namespace HelloWorld
 
         protected override void OnStart()
         {
+            AppCenter.Start("android=52521cf643bb5068bea9684ece5ba086d91f2b27;" +
+                  "ios=7c51ec670123e23f6577a8d8205852bedaab0494",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
